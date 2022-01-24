1314c5e6a47d5f06e586a08f0ac1b0664d9fa964
Atiq Israk <blueniil9@gmail.com>
2022-01-24 12:01:50 +0600

    button added to change title without function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,7 @@ import foodImage from '../../assets/img/cp.png';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import '../UI/ChangeButton.css';
 
 const ExpenseItem = (props) => {
     
@@ -10,9 +11,14 @@ const ExpenseItem = (props) => {
         <Card className="items">
           <div className="singleItem">
             <ExpenseDate date={props.date} />
-            <div className="justified-center"><img src={foodImage} alt="food item" /></div>
+            <div className="justified-center">
+              <img src={foodImage} alt="food item" />
+            </div>
             <h2 className="itemName justified-start">{props.title}</h2>
             <h2 className="itemPrice justified-center">BDT {props.amount}</h2>
+            <div className="buttonHolder justified-center">
+              <button className="chng">Change Title</button>
+            </div>
           </div>
         </Card>
       </div>
